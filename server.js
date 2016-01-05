9d2a83e17b0d8725b3deca3820eaf9919005503e
Claudio Canales V <claudio.canales@eftgroup.net>
2016-01-05 16:11:52 -0300

    global

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h2>Very nice :)</h2>');
+    res.end('<h2>Very nice :D</h2>');
 }).listen(process.env.PORT);
